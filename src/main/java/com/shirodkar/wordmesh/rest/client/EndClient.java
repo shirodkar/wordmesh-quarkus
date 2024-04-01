22e5678b6546ba4276110491f8c18d2299266661
shirodkar <shirodkar.sachin@gmail.com>
2024-04-01 13:19:51 -0400

    using config for endpoint urls

--- src/main/java/com/shirodkar/wordmesh/rest/client/EndClient.java
+++ src/main/java/com/shirodkar/wordmesh/rest/client/EndClient.java
@@ -1,12 +1,9 @@
 package com.shirodkar.wordmesh.rest.client;
 
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
 @Path("/wordmesh/end/word/{word}")
-@RegisterRestClient(baseUri="http://end:8080")
 public interface EndClient {
 
     @GET
