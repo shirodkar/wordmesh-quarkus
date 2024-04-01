49b97346116a31a56e354a3e3fdfa0557c3ab7ae
shirodkar <shirodkar.sachin@gmail.com>
2024-03-31 23:38:22 -0400

    first commit

---
+++ src/test/java/com/shirodkar/wordmesh/WordMeshResourceIT.java
@@ -0,0 +1,8 @@
+package com.shirodkar.wordmesh;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class WordMeshResourceIT extends WordMeshResourceTest {
+    // Execute the same tests but in packaged mode.
+}
